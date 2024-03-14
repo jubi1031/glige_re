b85ab11a3850e391bc28723086f0912373c9c034
jubi <joobe1031@gmail.com>
2024-03-14 16:20:36 +0900

    mainVisual

---
+++ js/common.js
@@ -0,0 +1,16 @@
+let swiper = new Swiper(".mainSlide", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".next1",
+    prevEl: ".prev1",
+  },
+  pagination: {
+    el: ".pg1",
+    clickable: true,
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
